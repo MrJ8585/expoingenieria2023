39c99f76ff2d858b363c3eee559d2ee31349a3d3
Calebpyn <87716503+Calebpyn@users.noreply.github.com>
2023-03-26 17:31:26 -0700

    Main components added

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Proyects from './Proyects'
+import Sections from './Sections'
+
+function Homepage() {
+  return (
+    <div className='home-page-main'>
+
+        <Proyects/>
+
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/components/Info.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../css/Info.css'
+
+function Info() {
+  return (
+    <div className='about-section'>
+
+        <div className='inner-content'>
+
+            <p id='upper-title'>¿Qué es?</p>
+            <p id='title'>Expo<b>Ingeniería</b></p>
+
+            <p id='content'>Es el evento más importante de la Escuela de Ingeniería, donde los alumnos tienen la <br/>
+            oportunidad de demostrar los conocimientos que están aprendiendo en el aula a través de<br/> 
+            proyectos de aplicación, con innovación y creatividad resolviendo problemas reales, y eso <br/>
+            les genera habilidades que van a aplicar cuando egresen y se inserten al campo laboral.</p>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Info
