9769b6ce64858bba2c3af4a79be0da2cd6602caa
Calebpyn <87716503+Calebpyn@users.noreply.github.com>
2023-04-03 09:57:09 -0700

    Some features added

--- src/components/Proyects.tsx
+++ src/components/Proyects.tsx
@@ -7,7 +7,7 @@ function Proyects() {
 
         <div className='inner-box'>
 
-            <img id='logo-exping' src='https://s3-alpha-sig.figma.com/img/020f/ee11/355e69c9b28c751a6766b926fd9f67f2?Expires=1680480000&Signature=QRb4uWw91YoaqJxWIQGS5MYtgXU5nsgLAirYbSHeF-lmF27-6mLMppEMZDHQDYMy84t~9sZXgWrTBKPPYAhyXF4OA1aefo5vHpA47j1gWuY1G4bU2YWzHQwn1kCylGLRqRHomRMS58skUxBx5~tGVZ3Iudh6MfHYPYq4XCU2WrUqZ3EmQopc6Va70oL2hy9lB0w7WLex7Erd~4jS7YrB4vIyrTjgtcZunYjuFd0yexkBSpK6b3phzKbNfXu441dPnC4xafk5odT65DnjkUGhcvGQ6SbZsDRqpQk2KtYA5RimqvLqtFVO~yNEo2pxBuyHjNN-WA-GgLV8ROf33L9BEQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' alt='logo' />
+            <img id='logo-exping' src='https://s3-alpha-sig.figma.com/img/020f/ee11/355e69c9b28c751a6766b926fd9f67f2?Expires=1681689600&Signature=hlc00KyDz9b7pT4p1FZ9AHu3eOFPB0EsAsMBsYKNNP1sn2fO9zjgBDUTV2tdxaWFf4a1tH9cppv2BjVcWziOWC8HNbpQ8G48B6awenRYRTKZyd3VA1p9uCNd200D4BdcuBKIrekvZ9w-AdSSodqBdpLgfZwhhGWT1gNiJL2rA12bm8-dWkECqLldpaFkyXgOMYJ5ZpB6qzAWBqA9rz5nRhhobb2LQzV6nIetHOf8vnK28w6YguHEmYupUMV-eU7hzaLSi4WV5aoFD3kr9i3IzQLV-IWjehJ2JEDdjHBDuotYfXiBRA7GKrACF12A2zLMVndNZwNCRtuG2Z382TK7dQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' alt='logo' />
 
 
             <p id='main-text'>Conoce ya los proyectos realizados por los alumnos de la Escuela de Ingeniería de <br/> 
